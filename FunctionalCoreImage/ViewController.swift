e72a2b2fb6342003dd3c9d1f7524d0e22e53aef3
“jrochabrun” <james.rochabrun@oath.com>
2018-11-13 09:04:49 -0800

    Initial Commit

---
+++ FunctionalCoreImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunctionalCoreImage
+//
+//  Created by James Rochabrun on 11/13/18.
+//  Copyright © 2018 James Rochabrun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
